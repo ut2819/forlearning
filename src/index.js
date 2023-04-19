28f46e9e75605be59457427e1c716eeffb1bfd0a
Utkarsh <uchaudhary974@gmail.com>
2023-04-20 01:07:27 +0530

    Greeting

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import "./index.css";
-const name='utkarsh';
-const img1='https://picsum.photos/200';
-const img2='https://picsum.photos/200';
-const img3='https://picsum.photos/200';
-const link="https://www.thapatechnical.com/";
 
+let currDate=new Date();
+currDate=currDate.getHours();
+const cssStyle={ };
+let greeting="";
+if(currDate>=0 && currDate<12){
+  greeting="Good Morning";
+  cssStyle.color="green";
+}else if(currDate>=12 && currDate<21){
+  greeting="Good Afternon";
+  cssStyle.color="Orange";
+}else{
+  greeting="Good Night";
+  cssStyle.color="Black";
+}
 ReactDOM.render(
   <>
-  <h1 style={{color: "#fa9191",textAlign: "center",textTransform: "capitalize",textShadow:"0px,2px,4px,#ffe9c5",fontFamily:"'Source Serif Pro', serif",fontWeight: "bold"}}>My Name is {name}</h1>
-  <div className='img'>
-  <img src={img1} alt='random img'/>
-  <img src={img2} alt='random img'/>
-   <a href={link} target='_blank'>
-  <img src={img3} alt='random img'/>
-   </a>
-   </div>
-   </>,
-  document.getElementById('root')
+  <div>
+ <h1> Hello sir,<span style={cssStyle}> {greeting}</span></h1>
+ </div>
+ </>,
+ document.getElementById("root")
 );
-
